20a3a488e532ad6b0785f39af1b70d4134c16987
nothink <gakongakon@gmail.com>
2022-10-11 14:45:34 +0900

    fix: first implementation using slack web api

--- functions/src/handler/storage.ts
+++ functions/src/handler/storage.ts
@@ -1,6 +1,17 @@
 import * as functions from 'firebase-functions/v1'
+import { App } from '@slack/bolt'
+import { ChatPostMessageArguments } from '@slack/web-api'
 
+const config = functions.config()
 const logger = functions.logger
+
+const boltApp = new App({
+  token: config.slack.bot_token,
+  signingSecret: config.slack.signing_secret,
+  processBeforeResponse: true
+})
+
+const RESOURCE_CHANNEL_ID = 'C02FSAAA8AF'
 
 /**
  * Cloud Storage Handler
@@ -9,6 +20,14 @@ const logger = functions.logger
 export const notifyHandler = async (
   object: functions.storage.ObjectMetadata
 ) => {
-  logger.info('link: ', object.mediaLink)
+  const filepath = `https://storage.googleapis.com/${object.bucket}/${object.name}`
+  const chat = boltApp.client.chat
+  const arg: ChatPostMessageArguments = {
+    channel: RESOURCE_CHANNEL_ID,
+    text: filepath
+  }
+  await chat.postMessage(arg)
+  logger.info('media link: ', object.mediaLink)
+  logger.info('self link: ', object.selfLink)
   logger.info('name: ', object.name)
 }
