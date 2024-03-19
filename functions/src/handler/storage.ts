55cc9919dacc8140df91dd7dbc3f409de9dc0997
nothink <gakongakon@gmail.com>
2024-03-19 16:33:16 +0900

    fix: rename notify to notifyV1

--- functions/src/handler/storage.ts
+++ functions/src/handler/storage.ts
@@ -1,4 +1,6 @@
 import * as functions from 'firebase-functions/v1'
+// import { StorageEvent } from 'firebase-functions/v2/storage'
+
 import { App } from '@slack/bolt'
 import { ChatPostMessageArguments } from '@slack/web-api'
 
@@ -16,7 +18,7 @@ const boltApp = new App({
  * Cloud Storage Handler
  * @param object Object Metadata
  */
-export const notifyHandler = async (
+export const notifyV1Handler = async (
   object: functions.storage.ObjectMetadata
 ) => {
   const filepath = `https://storage.googleapis.com/${object.bucket}/${object.name}`
@@ -28,3 +30,14 @@ export const notifyHandler = async (
   await chat.postMessage(arg)
   Logger.info('fetched file: ', filepath)
 }
+
+// export const notifyV2Handler = (event: StorageEvent) => {
+//   const filepath = `https://storage.googleapis.com/${event.data.bucket}/${event.data.name}`
+//   const chat = boltApp.client.chat
+//   const arg: ChatPostMessageArguments = {
+//     channel: FunctionInfo.RESOURCE_CHANNEL_ID,
+//     text: filepath
+//   }
+//   chat.postMessage(arg)
+//   Logger.info('fetched file: ', filepath)
+// }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,10 @@
 import * as functions from 'firebase-functions/v1'
 
+// v2 移行は functions.config() 以外で 'asia-northeast1' が指定できるまでお預け
+// import { onObjectFinalized, } from 'firebase-functions/v2/storage'
+
 import { dqx9mbrpz1jhxHandler } from './handler/https'
-import { notifyHandler } from './handler/storage'
+import { notifyV1Handler } from './handler/storage'
 import { FunctionInfo } from './globals'
 
 // dqx9mbrpz1jhx
@@ -14,7 +17,7 @@ export const dqx9mbrpz1jhx = functions
   .https.onRequest(dqx9mbrpz1jhxHandler)
 
 // notify
-export const notify = functions
+export const notifyV1 = functions
   .region('asia-northeast1')
   .runWith({
     memory: FunctionInfo.MEMORY,
@@ -22,10 +25,6 @@ export const notify = functions
   })
   .storage.bucket(FunctionInfo.BUCKET_NAME)
   .object()
-  .onFinalize(notifyHandler)
+  .onFinalize(notifyV1Handler)
 
-// export const kickNotify = functions
-//   .region('asia-northeast1')
-//   .runWith({ memory: '256MB', timeoutSeconds: 540 })
-//   .database.ref(Info.NAME)
-//   .onCreate(kickNotifyHandler)
+// export const notifyV2 = onObjectFinalized(FunctionInfo.BUCKET_NAME, notifyV2Handler)
