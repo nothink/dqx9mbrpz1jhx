986d2d0c18ba1c3a38b667131a97b18e1205577a
nothink <gakongakon@gmail.com>
2024-04-02 17:24:16 +0900

    fix: separate channnel ID of Slack to envs

--- functions/src/globals.ts
+++ functions/src/globals.ts
@@ -8,9 +8,6 @@ export const Logger = logger;
 /** bucket name */
 export const BUCKET_NAME = "dqx9mbrpz1jhx";
 
-/** Slack resource channnel ID for send notifications to */
-export const RESOURCE_CHANNEL_ID = "C02FSAAA8AF";
-
 /**
  * v2 global options.
  */
--- functions/src/handler/storage.ts
+++ functions/src/handler/storage.ts
@@ -3,16 +3,16 @@ import type { StorageEvent } from "firebase-functions/v2/storage";
 
 import { App } from "@slack/bolt";
 
-import { Logger, RESOURCE_CHANNEL_ID } from "../globals";
+import { Logger } from "../globals";
 
 // v2 config
-const slackBotToken = defineString("SLACK_BOT_TOKEN").value();
-const slackSigningSecret = defineString("SLACK_SIGNING_SECRET").value();
-const slackChannel = RESOURCE_CHANNEL_ID;
+const slackBotToken = defineString("SLACK_BOT_TOKEN");
+const slackSigningSecret = defineString("SLACK_SIGNING_SECRET");
+const slackChannel = defineString("SLACK_CHANNEL_ID");
 
 const boltAppV2 = new App({
-	token: slackBotToken,
-	signingSecret: slackSigningSecret,
+	token: slackBotToken.value(),
+	signingSecret: slackSigningSecret.value(),
 	processBeforeResponse: true,
 });
 
@@ -24,7 +24,7 @@ export const notifyV2Handler = (event: StorageEvent) => {
 	const url = `https://storage.googleapis.com/${event.data.bucket}/${event.data.name}`;
 	const chat = boltAppV2.client.chat;
 	chat.postMessage({
-		channel: slackChannel,
+		channel: slackChannel.value(),
 		text: url,
 	});
 	Logger.info("fetched file: ", url);
