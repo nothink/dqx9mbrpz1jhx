fdd7a2746f0758b152652a4e0cc0e544249db188
nothink <gakongakon@gmail.com>
2024-04-05 12:41:07 +0900

    fix: update packages

--- functions/src/utils/discord.ts
+++ functions/src/utils/discord.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 
 import { DISCORD_WEBHOOK_URL } from "../globals";
 
+/**
+ * DiscordにWebhook経由で通知する
+ * 	https://discord.com/developers/docs/resources/webhook#execute-webhook-jsonform-params
+ * @param url URL
+ */
 export const notifyDiscord = (url: URL) => {
 	const message = {
 		content: url.href,
